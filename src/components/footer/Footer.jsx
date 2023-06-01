903ffb3602310bc65628d356995d1a9cd3030310
Apaar Dahal <contact.apaardahal@gmail.com>
2023-06-01 17:02:22 +0545

    navbar and footer completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,43 @@
 import React from "react"
+import styles from "./page.module.css"
+import Image from "next/image"
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className={styles.container}>
+      <div>@2023 Apaar. All rights reserved</div>
+      <div className={styles.social}>
+        <Image
+          src="/1.png"
+          width={15}
+          height={15}
+          className={styles.socialIcon}
+          alt="facebook"
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          height={15}
+          className={styles.socialIcon}
+          alt="instagram"
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          className={styles.socialIcon}
+          alt="twitter"
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          className={styles.socialIcon}
+          alt="apaar"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Footer
