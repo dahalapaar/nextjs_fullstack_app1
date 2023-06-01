ba1e58d989bc047462bfd831124cca5e034fab6d
Apaar Dahal <contact.apaardahal@gmail.com>
2023-06-01 18:02:50 +0545

    about me page completed

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,51 @@
 import React from "react"
 import styles from "./page.module.css"
+import Image from "next/image"
+import Button from "@/components/button/Button"
 
 const About = () => {
-  return <div className={styles.container}>About</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/17035440/pexels-photo-17035440/free-photo-of-love-photographer-autumn-portrait.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          fill={true}
+          alt=""
+          className={styles.img}
+        />
+        <div className={styles.imgText}>
+          <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+          <h2 className={styles.imgDesc}>Award winning digital experiences</h2>
+        </div>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Who Am I?</h1>
+          <p className={styles.desc}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            repellat totam similique optio nulla, quaerat maxime consequatur
+            necessitatibus praesentium dignissimos unde autem perferendis rerum
+            enim laudantium debitis minus inventore eos!
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            quisquam, accusamus perferendis mollitia magni eius distinctio quos
+            quas corrupti ab eum nulla quod eaque earum, atque similique iste
+            itaque odio.
+          </p>
+        </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>What I do?</h1>
+          <p className={styles.desc}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            repellat totam similique optio nulla, quaerat maxime consequatur
+            necessitatibus praesentium dignissimos unde autem perferendis rerum
+            enim laudantium debitis minus inventore eos!
+          </p>
+          <Button url="contact" text="Contact" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default About
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import styles from "./page.module.css"
 import Hero from "public/hero.png"
+import Button from "@/components/button/Button"
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
           Turning your ideas into Reality. We bring together the teams from the
           global tech industry.
         </p>
-        <button className={styles.button}>See My Works</button>
+        <Button url="/portfolio" text="See My Works" />
       </div>
       <div className={styles.item}>
         <Image src={Hero} alt="mainImage" className={styles.img} />
