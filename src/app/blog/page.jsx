67e32a0ceb7a6d192edaba18b345a26517ecaa61
Apaar Dahal <contact.apaardahal@gmail.com>
2023-06-03 17:40:58 +0545

    feat: server side data fetching implemented

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,55 +3,39 @@ import styles from "./page.module.css"
 import Link from "next/link"
 import Image from "next/image"
 
-const Blog = () => {
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    caches: "no-store",
+  })
+  if (!res.ok) {
+    throw new Error("Failed to fetch data")
+  }
+
+  return res.json()
+}
+
+const Blog = async () => {
+  const data = await getData()
+
   return (
     <div className={styles.mainContainer}>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/9435632/pexels-photo-9435632.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            width={400}
-            height={400}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
-
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/9435632/pexels-photo-9435632.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            width={400}
-            height={400}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/9435632/pexels-photo-9435632.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            width={400}
-            height={400}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
+      {data.map((item) => (
+        <Link href="/blog/testId" className={styles.container} key={item.id}>
+          <div className={styles.imgContainer}>
+            <Image
+              src="https://images.pexels.com/photos/9435632/pexels-photo-9435632.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              width={400}
+              height={400}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>Desc</p>
+          </div>
+        </Link>
+      ))}
     </div>
   )
 }
