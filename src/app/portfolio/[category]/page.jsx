031f8b8c00ad6a2fba9096251ca3e7e68e44be1c
Apaar Dahal <contact.apaardahal@gmail.com>
2023-06-14 14:50:11 +0545

    feat: schema created

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,41 +2,34 @@ import React from "react"
 import styles from "./page.module.css"
 import Button from "@/components/button/Button"
 import Image from "next/image"
+import { items } from "./data"
+import { notFound } from "next/navigation"
+
+const getData = (cat) => {
+  const data = items[cat]
+  if (data) {
+    return data
+  }
+  return notFound
+}
 
 const Category = ({ params }) => {
+  const data = getData(params.category)
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button text="See More" url="#" />
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text="See More" url="#" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image src={item.image} alt="" fill={true} className={styles.img} />
+          </div>
         </div>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/16952093/pexels-photo-16952093/free-photo-of-young-woman-in-a-hat-against-a-clear-blue-sky.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            fill={true}
-            className={styles.img}
-          />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button text="See More" url="#" />
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/16952093/pexels-photo-16952093/free-photo-of-young-woman-in-a-hat-against-a-clear-blue-sky.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            fill={true}
-            className={styles.img}
-          />
-        </div>
-      </div>
+      ))}
     </div>
   )
 }
